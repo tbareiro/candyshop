23e9eb920856f169997a4d119549f101253232d1
tbareiro <tbareiro98@gmail.com>
2022-08-23 17:50:05 -0300

    funciones

--- app.js
+++ app.js
@@ -11,92 +11,7 @@ let producto3 = new Productos({nombre: "Bolsa de 600gr de gomitas", precio: 750,
 
 let carrito = []
 let total = 0
-
-
-function sumaProducto1(){
-    let opcion = confirm("Queres agregar "+producto1.nombre+"? El valor es de $"+producto1.precio+" y contiene: "+producto1.contenido+".")
-    if(opcion == true){
-        total = total + producto1.precio;
-        carrito.push(producto1.contenido)
-        alert(producto1.nombre+" agregado al carrito!")
-    }
-}
-
-function sumaProducto2(){
-    let opcion = confirm("Queres agregar "+producto2.nombre+"? El valor es de $"+producto2.precio+" y contiene: "+producto2.contenido+".")
-    if(opcion == true){
-        total = total + producto2.precio;
-        carrito.push(producto2.contenido)
-        alert("Producto agregado al carrito!")
-    }
-}
-
-function sumaProducto3(){
-    let opcion = confirm("Queres agregar "+producto3.nombre+"? El valor es de $"+producto3.precio+" y contiene: "+producto3.contenido+".")
-    if(opcion == true){
-        total = total + producto3.precio;
-        carrito.push(producto3.contenido)
-        alert("Producto agregado al carrito!")
-    }
-}
-
-function envio(){
-    if(total <= 1099){
-        total = total + 300
-        alert("Se sumaran $300 de envio. La compra minima para el envio gratis es de $1099.")
-    } else {
-        alert("Contas con envio gratis en esta compra!")
-    }
-}
-
-sumaProducto1();
-sumaProducto2();
-sumaProducto3();
-envio();
-
-totalCarrito = []
-
-carrito.forEach(function(nombre) {
-    totalCarrito.push(nombre);
-})
-
-function carritoCompra() {
-    alert("Has seleccionado "+carrito.length+" articulos. "+totalCarrito)
-}
-
-carritoCompra();
-
-function pagos(){
-    medioDePago = prompt("El total es $" + total + " Ingrese como quiere abonar (efectivo, tarjeta de debito, tarjeta de credito o transferencia)")
-    switch (medioDePago){
-        case "efectivo": 
-            return alert("Perfecto! El total es $" + total + ". Seras redirigido a la pagina de pago.");
-
-        case "tarjeta de debito": 
-            return alert("Perfecto! El total es $" + total + ". Seras redirigido a la pagina de pago.");
-            
-        case "debito": 
-            return alert("Perfecto! El total es $" + total + ". Seras redirigido a la pagina de pago.");
-
-        case "credito": 
-            total = total * 1.15
-            return alert("Perfecto! Se aplicara un recargo del 15%. El total es $" + total.toFixed(2) + ". Seras redirigido a la pagina de pago.")
-
-        case "tarjeta de credito": 
-            total = total * 1.15
-            return  alert("Perfecto! Se aplicara un recargo del 15%. El total es $" + total.toFixed(2) + ". Seras redirigido a la pagina de pago.")
-
-        case "transferencia": 
-            return alert("Perfecto! El total es $" + total + ". Seras redirigido a la pagina de pago.")
-        
-        default:
-            alert("El medio de pago ingresado no es valido. Vuelva a intentarlo.")
-            pagos();
-            break;
-    }
-}
-
-pagos();
+let totalCarrito = document.getElementById("total")
 
 let titulo1 = document.getElementById("producto1-titulo")
 titulo1.innerText = producto1.nombre
@@ -116,17 +31,54 @@ precio2.innerText = producto2.precio
 let precio3 = document.getElementById("producto3-precio")
 precio3.innerText = producto3.precio
 
+let carritoContainer = document.getElementById("carrito")
 
-let boton = document.getElementsByTagName("button")
+let boton1 = document.getElementById("button1")
+boton1.addEventListener("click", agregar1)
 
-for (i = 0; i < boton.length; i++) {
-    boton[i].addEventListener("click", agregar);
+function agregar1(){
+    carrito.push(producto1.nombre)
+    carritoContainer.innerText = carrito
+    total = total + producto1.precio
+    totalCarrito.innerText = "El total es: $"+ total 
 }
 
-function agregar(){
-    carrito.push("Item")
-    console.log(carrito)
-    let parrafo = document.createElement("p")
-    parrafo.innerText = "Agregado al carrito!"
-    document.body.append(parrafo)
+let boton2 = document.getElementById("button2")
+boton2.addEventListener("click", agregar2)
+
+function agregar2(){
+    carrito.push(producto2.nombre)
+    carritoContainer.innerText = carrito
+    total = total + producto2.precio
+    totalCarrito.innerText = "El total es: $"+ total 
+}
+
+let boton3 = document.getElementById("button3")
+boton3.addEventListener("click", agregar3)
+
+function agregar3(){
+    carrito.push(producto3.nombre)
+    carritoContainer.innerText = carrito
+    total = total + producto3.precio
+    totalCarrito.innerText = "El total es: $"+ total 
+}
+
+let botonPago = document.getElementById("pago")
+botonPago.addEventListener("click", envio)
+
+let costoEnvio = 300
+
+function envio(){
+    if(total === 0){
+        alert ("Aun no has sumado nada al carrito!")
+    } else if(total <= 1999){
+        total2 = total + costoEnvio
+        alert("Se sumaran $300 de envio. La compra minima para el envio gratis es de $1999.")
+        totalCarrito.innerText = "El total es: $"+ total2
+    } else {
+        costoEnvio = 0
+        total = total + costoEnvio
+        textoEnvio = document.getElementById("costo-envio")
+        textoEnvio.innerText = "Contas con envio gratis en esta compra!"
+    }
 }
